38538b7a2b0a8343ea0a31a1a866cf0009dde8af
Aleksander <filipkowskialek@gmail.com>
2025-03-12 21:56:15 +0100

    database adjustments

---
+++ PhoneBook/Migrations/20250311112151_ReversedChange.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoneBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReversedChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_PhoneNumber",
+                table: "Contacts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Contacts",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_PhoneNumber",
+                table: "Contacts",
+                column: "PhoneNumber",
+                unique: true);
+        }
+    }
+}
